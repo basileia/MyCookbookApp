2fa5ba9e83192e5f9c9e0be169ff8140c0c162a8
basileia <jane.novak@centrum.cz>
2025-01-22 09:30:46 +0100

    Added Ingredient model, seed categories

--- MyCookbook/MyCookbook/Data/ApplicationDbContext.cs
+++ MyCookbook/MyCookbook/Data/ApplicationDbContext.cs
@@ -7,5 +7,18 @@ namespace MyCookbook.Data
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
         public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Snídanì" },
+                new Category { Id = 2, Name = "Obìd" },
+                new Category { Id = 3, Name = "Veèeøe" },
+                new Category { Id = 4, Name = "Svaèina" }
+            );
+        }
     }
 }
---
+++ MyCookbook/MyCookbook/Data/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace MyCookbook.Data.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
