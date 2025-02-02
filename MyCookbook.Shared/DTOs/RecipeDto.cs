c46bc73c0a64a27acf28aa67fa624888dd71edd0
basileia <jane.novak@centrum.cz>
2025-02-02 21:42:09 +0100

    added CategoryDto

--- MyCookbook.Shared/DTOs/RecipeDto.cs
+++ MyCookbook.Shared/DTOs/RecipeDto.cs
@@ -1,6 +1,15 @@
 ﻿namespace MyCookbook.Shared.DTOs
 {
-    internal class RecipeDto
+    public class RecipeDto
     {
+        //public int Id { get; set; }
+        //public string Name { get; set; }
+        //public int NumberOfServings { get; set; }
+        //public string Preparation { get; set; }
+        //public DateTime DateAdded { get; set; }
+        //public string UserId { get; set; }
+        //public string UserName { get; set; }  
+        //public List<CategoryDto> Categories { get; set; }
+        //public List<RecipeIngredientDto> Ingredients { get; set; }
     }
 }
