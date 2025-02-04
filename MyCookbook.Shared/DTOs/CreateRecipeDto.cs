938e76842660eac97a7f5228c346e1a3a94e5dba
basileia <jane.novak@centrum.cz>
2025-02-04 14:32:08 +0100

    added CreateRecipeDto

---
+++ MyCookbook.Shared/DTOs/CreateRecipeDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyCookbook.Shared.DTOs
+{
+    public class CreateRecipeDto
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int NumberOfServings { get; set; }
+
+        [Required]
+        public string Preparation { get; set; }
+
+        public List<int> CategoryIds { get; set; } 
+        public List<CreateRecipeIngredientDto> Ingredients { get; set; }
+    }
+}
