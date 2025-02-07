a45bb90e497d3dfca55581121a6780f4aeceb01c
basileia <jane.novak@centrum.cz>
2025-02-07 18:52:14 +0100

    added UserRecipeListDto

---
+++ MyCookbook.Shared/DTOs/UserRecipeListDto.cs
@@ -0,0 +1,10 @@
+﻿using MyCookbook.Shared.DTOs.RecipeDTOs;
+
+namespace MyCookbook.Shared.DTOs
+{
+    public class UserRecipeListDto
+    {
+        public string UserId { get; set; }
+        public List<RecipeDto> Recipes { get; set; }
+    }
+}
