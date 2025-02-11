480aeba35aa0505d7cdfedcf6793c673da97032d
basileia <jane.novak@centrum.cz>
2025-02-11 09:28:35 +0100

    added BaseRepository

--- MyCookbook/MyCookbook/Data/Repositories/BaseRepository.cs
+++ MyCookbook/MyCookbook/Data/Repositories/BaseRepository.cs
@@ -1,6 +1,45 @@
-﻿namespace MyCookbook.Data.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using MyCookbook.Data.Contracts;
+
+namespace MyCookbook.Data.Repositories
 {
-    public class BaseRepository
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
     {
+        protected readonly ApplicationDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public BaseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
     }
 }
