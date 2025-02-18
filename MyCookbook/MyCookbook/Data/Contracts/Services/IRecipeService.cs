22e5e84d1d6af0e9cc1a278daa6aa1cc0475f549
basileia <jane.novak@centrum.cz>
2025-02-18 17:11:56 +0100

    added IRecipeService

--- MyCookbook/MyCookbook/Data/Contracts/Services/IRecipeService.cs
+++ MyCookbook/MyCookbook/Data/Contracts/Services/IRecipeService.cs
@@ -1,6 +1,10 @@
-﻿namespace MyCookbook.Data.Contracts.Services
+﻿using MyCookbook.Shared.DTOs.RecipeDTOs;
+
+namespace MyCookbook.Data.Contracts.Services
 {
     public interface IRecipeService
     {
+        Task<List<RecipeListDto>> GetAllRecipesAsync();
+        Task<RecipeDetailDto?> GetRecipeByIdAsync(int id);
     }
 }
