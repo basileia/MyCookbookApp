279e6e2e72d9491fd0d774453b64a763e17f60e1
basileia <jane.novak@centrum.cz>
2025-02-18 17:10:20 +0100

    added RecipeRepository

---
+++ MyCookbook/MyCookbook/Data/Repositories/RecipeRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyCookbook.Data.Contracts.Repositories;
+using MyCookbook.Data.Models;
+
+namespace MyCookbook.Data.Repositories
+{
+    public class RecipeRepository : BaseRepository<Recipe>, IRecipeRepository
+    {
+        public RecipeRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<List<Recipe>> GetAllWithCategoriesAsync()
+        {
+            return await _context.Recipes
+                .Include(r => r.Categories)
+                .ToListAsync();
+        }
+
+        public async Task<Recipe?> GetByIdWithDetailsAsync(int id)
+        {
+            return await _context.Recipes
+                .Include(r => r.Categories)
+                .Include(r => r.Ingredients)
+                .FirstOrDefaultAsync(r => r.Id == id);
+        }
+    }
+}
