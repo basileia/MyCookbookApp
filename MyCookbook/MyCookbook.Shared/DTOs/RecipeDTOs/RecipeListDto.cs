aaf40130bac995e260cdf67854435912b160dfc4
basileia <jane.novak@centrum.cz>
2025-02-18 16:59:55 +0100

    RecipeListDto (Icon -> Icons)

--- MyCookbook/MyCookbook.Shared/DTOs/RecipeDTOs/RecipeListDto.cs
+++ MyCookbook/MyCookbook.Shared/DTOs/RecipeDTOs/RecipeListDto.cs
@@ -5,15 +5,18 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public List<CategoryDto> Categories { get; set; }
-        public string Icon => GetCategoryIcon();
+        public List<string> Icons => GetCategoryIcons();
 
-        private string GetCategoryIcon()
+        private List<string> GetCategoryIcons()
         {
-            if (Categories.Any(c => c.Name == "Snídaně")) return "fa-solid fa-bread-slice";
-            if (Categories.Any(c => c.Name == "Oběd")) return "fa-solid fa-utensils";
-            if (Categories.Any(c => c.Name == "Večeře")) return "fa-solid fa-bowl-food";
-            if (Categories.Any(c => c.Name == "Svačina")) return "fa-solid fa-mug-hot";
-            return "fa-solid fa-question";
+            List<string> icons = new List<string>();
+
+            if (Categories.Any(c => c.Name == "Snídaně")) icons.Add("fa-solid fa-bread-slice");
+            if (Categories.Any(c => c.Name == "Oběd")) icons.Add("fa-solid fa-utensils");
+            if (Categories.Any(c => c.Name == "Večeře")) icons.Add("fa-solid fa-bowl-food");
+            if (Categories.Any(c => c.Name == "Svačina")) icons.Add("fa-solid fa-mug-hot");
+
+            return icons.Any() ? icons : new List<string> { "fa-solid fa-question" };
         }
     }
 }
