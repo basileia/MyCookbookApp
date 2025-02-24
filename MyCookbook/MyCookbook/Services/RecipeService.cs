a6a1cdfcd913a64bedb4232a18b7b54e11d49a2a
basileia <jane.novak@centrum.cz>
2025-02-24 09:46:18 +0100

    added RecipeService

--- MyCookbook/MyCookbook/Services/RecipeService.cs
+++ MyCookbook/MyCookbook/Services/RecipeService.cs
@@ -1,9 +1,31 @@
-﻿//using MyCookbook.Data.Contracts.Services;
+﻿using AutoMapper;
+using MyCookbook.Data.Contracts.Repositories;
+using MyCookbook.Data.Contracts.Services;
+using MyCookbook.Shared.DTOs.RecipeDTOs;
 
-//namespace MyCookbook.Services
-//{
-//    public class RecipeService : IRecipeService
-//    {
+namespace MyCookbook.Services
+{
+    public class RecipeService : IRecipeService
+    {
+        private readonly IRecipeRepository _recipeRepository;
+        private readonly IMapper _mapper;
 
-//    }
-//}
+        public RecipeService(IRecipeRepository recipeRepository, IMapper mapper)
+        {
+            _recipeRepository = recipeRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<RecipeListDto>> GetAllRecipesAsync()
+        {
+            var recipes = await _recipeRepository.GetAllWithCategoriesAsync();
+            return _mapper.Map<List<RecipeListDto>>(recipes);
+        }
+
+        public async Task<RecipeDetailDto?> GetRecipeByIdAsync(int id)
+        {
+            var recipe = await _recipeRepository.GetByIdWithDetailsAsync(id);
+            return recipe == null ? null : _mapper.Map<RecipeDetailDto>(recipe);
+        }
+    }
+}
