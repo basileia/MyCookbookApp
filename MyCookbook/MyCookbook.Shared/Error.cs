baf5732268bd1b10ebec1550a6f02351c39b4aaa
basileia <jane.novak@centrum.cz>
2025-03-07 14:32:07 +0100

    added Error

---
+++ MyCookbook/MyCookbook.Shared/Error.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+namespace MyCookbook.Shared
+{
+    public class Error
+    {
+        public string Code { get; set; }  
+        public string? Details { get; set; }
+
+        public Error(string code, string? details = null)
+        {
+            Code = code;
+            Details = details;
+        }
+    }
+}
