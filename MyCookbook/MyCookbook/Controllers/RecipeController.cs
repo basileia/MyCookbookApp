c853900c6faed25742106974ebc291432338827d
basileia <jane.novak@centrum.cz>
2025-03-07 10:06:59 +0100

    RecipeController - delete recipe

--- MyCookbook/MyCookbook/Controllers/RecipeController.cs
+++ MyCookbook/MyCookbook/Controllers/RecipeController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using MyCookbook.Data.Contracts.Services;
 using MyCookbook.Shared.DTOs.RecipeDTOs;
+using System.Security.Claims;
 
 namespace MyCookbook.Controllers
 {
@@ -28,6 +30,33 @@ namespace MyCookbook.Controllers
             var recipe = await _recipeService.GetRecipeByIdAsync(id);
             if (recipe == null) return NotFound("Recept nebyl nalezen");
             return Ok(recipe);
+        }
+
+        [Authorize]
+        [HttpDelete("deleterecipe/{id}")]
+        public async Task<IActionResult> DeleteRecipe(int id)
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (string.IsNullOrEmpty(userId))
+            {
+                return Unauthorized("Uživatel není přihlášen.");
+            }
+
+            try
+            {
+                var success = await _recipeService.DeleteRecipeAsync(id, userId);
+                if (!success)
+                {
+                    return NotFound("Recept nebyl nalezen.");
+                }
+
+                return NoContent();
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return Forbid();
+            }
         }
     }
 }
