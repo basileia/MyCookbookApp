676db00bcfb45da53d85a098ea00ae2f5a3d5c58
basileia <jane.novak@centrum.cz>
2025-05-14 12:09:23 +0200

    Add IRecipeIngredientService interface for recipe ingredients

---
+++ MyCookbook/MyCookbook/Data/Contracts/Services/IRecipeIngredientService.cs
@@ -0,0 +1,12 @@
+﻿using LanguageExt;
+using MyCookbook.Results;
+using MyCookbook.Shared.DTOs.RecipeIngredientDTOs;
+
+namespace MyCookbook.Data.Contracts.Services
+{
+    public interface IRecipeIngredientService
+    {
+        Task<Result<Unit, string>> AddIngredientToRecipeAsync(int recipeId, CreateRecipeIngredientDto dto);
+        Task<Result<IEnumerable<RecipeIngredientDto>, string>> GetIngredientsForRecipeAsync(int recipeId);
+    }
+}
