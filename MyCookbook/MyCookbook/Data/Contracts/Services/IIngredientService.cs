8095d2b68eb6bea90081adab421b9aba7f242d59
basileia <jane.novak@centrum.cz>
2025-05-14 10:19:36 +0200

    Add IIngredientService interface for ingredient operations

---
+++ MyCookbook/MyCookbook/Data/Contracts/Services/IIngredientService.cs
@@ -0,0 +1,10 @@
+﻿using MyCookbook.Shared.DTOs.IngredientDTOs;
+
+namespace MyCookbook.Data.Contracts.Services
+{
+    public interface IIngredientService
+    {
+        Task<List<IngredientDto>> GetAllAsync();
+        Task<IngredientDto> AddAsync(CreateIngredientDto createIngredientDto);
+    }
+}
