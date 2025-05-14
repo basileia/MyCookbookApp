51096726f5065e2c7d95783eedc134df02573913
basileia <jane.novak@centrum.cz>
2025-05-14 12:02:13 +0200

    Add RecipeIngredientRepository with query methods

---
+++ MyCookbook/MyCookbook/Data/Repositories/RecipeIngredientRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyCookbook.Data.Contracts.Repositories;
+
+namespace MyCookbook.Data.Repositories
+{
+    public class RecipeIngredientRepository : BaseRepository<RecipeIngredient>, IRecipeIngredientRepository
+    {
+        public RecipeIngredientRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<RecipeIngredient>> GetByRecipeIdAsync(int recipeId)
+        {
+            return await _context.RecipeIngredients
+                .Include(ri => ri.Ingredient)
+                .Where(ri => ri.RecipeId == recipeId)
+                .ToListAsync();
+        }
+
+        public async Task<RecipeIngredient?> GetByIdsAsync(int recipeId, int ingredientId)
+        {
+            return await _context.RecipeIngredients
+                .FirstOrDefaultAsync(ri => ri.RecipeId == recipeId && ri.IngredientId == ingredientId);
+        }
+    }
+}
