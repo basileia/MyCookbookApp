b8ebbf0951e0d36d71e5473fa23e3daf104ae92d
basileia <jane.novak@centrum.cz>
2025-05-14 10:16:18 +0200

    Add IngredientRepository with GetAllAsync method

---
+++ MyCookbook/MyCookbook/Data/Repositories/IngredientRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyCookbook.Data.Contracts.Repositories;
+using MyCookbook.Data.Models;
+
+namespace MyCookbook.Data.Repositories
+{
+    public class IngredientRepository : BaseRepository<Ingredient>, IIngredientRepository
+    {
+        public IngredientRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<List<Ingredient>> GetAllAsync()
+        {
+            return await _context.Ingredients
+                .OrderBy(i => i.Name)
+                .ToListAsync();
+        }        
+    }
+}
