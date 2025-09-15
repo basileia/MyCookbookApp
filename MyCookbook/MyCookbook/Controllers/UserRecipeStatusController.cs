9a5a06524299d5cb6ab9d81dc439f59d371e114c
basileia <jane.novak@centrum.cz>
2025-09-15 10:45:58 +0200

    create UserRecipeStatusController

---
+++ MyCookbook/MyCookbook/Controllers/UserRecipeStatusController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyCookbook.Data.Contracts.Services;
+
+namespace MyCookbook.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserRecipeStatusController : BaseController
+    {
+        private readonly IUserRecipeStatusService _userRecipeStatusService;
+
+        public UserRecipeStatusController(IUserRecipeStatusService userRecipeStatusService)
+        {
+            _userRecipeStatusService = userRecipeStatusService;
+        }
+
+    }
+}
---
+++ MyCookbook/MyCookbook/Data/Contracts/Services/IUserRecipeStatusService.cs
@@ -0,0 +1,6 @@
+﻿namespace MyCookbook.Data.Contracts.Services
+{
+    public interface IUserRecipeStatusService
+    {
+    }
+}
