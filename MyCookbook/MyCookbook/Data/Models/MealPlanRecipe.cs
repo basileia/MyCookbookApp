3e6ec60dd92fbf73c0b2d4e5037618c67ddc12b2
basileia <jane.novak@centrum.cz>
2025-09-30 14:30:35 +0200

    add MealPlanRecipe entity

---
+++ MyCookbook/MyCookbook/Data/Models/MealPlanRecipe.cs
@@ -0,0 +1,11 @@
+﻿namespace MyCookbook.Data.Models
+{
+    public class MealPlanRecipe
+    {
+        public int MealPlanDayId { get; set; }
+        public MealPlanDay MealPlanDay { get; set; }
+
+        public int RecipeId { get; set; }
+        public Recipe Recipe { get; set; }
+    }
+}
