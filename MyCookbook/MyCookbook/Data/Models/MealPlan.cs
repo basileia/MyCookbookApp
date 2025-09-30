61f5b455915d42d524e1d9a5ff208bcf55c4ef9a
basileia <jane.novak@centrum.cz>
2025-09-30 14:26:22 +0200

    add MealPlanDay entity

--- MyCookbook/MyCookbook/Data/Models/MealPlan.cs
+++ MyCookbook/MyCookbook/Data/Models/MealPlan.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime CreatedAt { get; set; }
+        public DayOfWeek StartDayOfWeek { get; set; }
         public int Days { get; set; }
 
         public string UserId { get; set; }
